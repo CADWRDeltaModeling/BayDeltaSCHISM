1d3ebcb260ecb5f027646f28888f48d44bfab17c
Eli <Eli.Ateljevich@water.ca.gov>
2020-04-04 16:44:00 -0700

    RE worded

--- pyschism/prepare_schism.py
+++ pyschism/prepare_schism.py
@@ -83,14 +83,14 @@ def create_hgrid(s, inputs, logger):
         # Write hgrid.gr3
         option_name = 'gr3_outputfile'
         if option_name in section:
-            logger.info("Writing up a new hgrid file...")
+            logger.info("Writing hgrid file...")
             hgrid_out_fpath = os.path.expanduser(section[option_name])
             s.write_hgrid(hgrid_out_fpath)
 
         # Write hgrid.ll
         option_name = 'll_outputfile'
         if option_name in section:
-            logger.info("Creating a new hgrid.ll file...")
+            logger.info("Creating hgrid.ll file...")
             hgrid_ll_fpath = os.path.expanduser(section[option_name])
             s.write_hgrid_ll(hgrid_ll_fpath)
 
