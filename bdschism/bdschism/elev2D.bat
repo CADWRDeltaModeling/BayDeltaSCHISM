a364b707698f969c8f8e209bf57d7b18c88f88e1
Hans Kim <hansang.kim@water.ca.gov>
2023-07-12 10:43:54 -0700

    use lower case (function name in upper case is not recognized on HPC4)

--- bdschism/bdschism/elev2D.bat
+++ bdschism/bdschism/elev2D.bat
@@ -3,4 +3,4 @@ rem This needs to be edited to point to the actual location of the hgrid.gr3 fil
 
 download_noaa --stations sffpx mtyc1 pryc1 --syear 2009 --eyear 2011 --param elev --dest . || exit /b
 
-gen_elev2D --stime 2009-05-01 --etime 2011-01-01 --hgrid ../../bay_delta/hgrid.gr3 --outfile elev2D.2021.th.nc --slr 0.0 noaa_pryc1*elev*.csv noaa_mtyc1*elev*.csv
+gen_elev2d --stime 2009-05-01 --etime 2011-01-01 --hgrid ../../bay_delta/hgrid.gr3 --outfile elev2D.2021.th.nc --slr 0.0 noaa_pryc1*elev*.csv noaa_mtyc1*elev*.csv
