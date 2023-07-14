75312f66e2fae603848ec72a984cc7bde2d539e7
Kijin Nam <knam@water.ca.gov>
2023-07-14 21:31:22 +0000

    Add CLI HSI calculation codes

---
+++ bdschism/bdschism/calculate_hsi2.py
@@ -0,0 +1,230 @@
+"""
+"""
+
+from pathlib import Path
+import logging
+import argparse
+import datetime
+import numpy as np
+import pandas as pd
+import xarray as xr
+import suxarray as sx
+import suxarray.helper
+
+logging.basicConfig(
+    format="%(asctime)s %(levelname)s %(message)s",
+    datefmt="%Y-%m-%d %H:%M:%S",
+    level=logging.INFO,
+)
+
+
+def create_argparser():
+    argparser = argparse.ArgumentParser()
+    argparser.add_argument("--path_study", type=lambda s: Path(s))
+    argparser.add_argument("--path_common", type=lambda s: Path(s))
+    argparser.add_argument("--path_postprocess", default=".", type=lambda s: Path(s))
+    argparser.add_argument(
+        "--date_base", type=lambda s: datetime.datetime.strptime(s, "%Y-%m-%d")
+    )
+    argparser.add_argument(
+        "--date_start", type=lambda s: datetime.datetime.strptime(s, "%Y-%m-%d")
+    )
+    argparser.add_argument(
+        "--date_end", type=lambda s: datetime.datetime.strptime(s, "%Y-%m-%d")
+    )
+    return argparser
+
+
+def main():
+    argparser = create_argparser()
+    args = argparser.parse_args()
+
+    path_study = Path(args.path_study)
+    path_common = Path(args.path_common)
+    path_postprocess = Path(args.path_postprocess)
+    date_base = args.date_base
+    year = date_base.year
+    date_start = args.date_start
+    date_end = args.date_end
+
+    day_start = (date_start - date_base).days + 1
+    day_end = (date_end - date_base).days + 1
+
+    # sx_ds = read_out2d_and_create_grid(path_study, day_start, day_end)
+
+    logging.info("Reading postprocessed depth-averaged data...")
+    chunks = {"time": 48}
+    path_depth_averaged_salinity_at_face = (
+        path_postprocess / "depth_averaged_salinity_at_face.nc"
+    )
+    ds_depth_averaged_salinity_at_face = xr.open_dataset(
+        path_depth_averaged_salinity_at_face, chunks=chunks
+    )
+    path_depth_averaged_horizontalVelX_at_face = (
+        path_postprocess / "depth_averaged_horizontalVelX_at_face.nc"
+    )
+    ds_depth_averaged_horizontalVelX_at_face = xr.open_dataset(
+        path_depth_averaged_horizontalVelX_at_face, chunks=chunks
+    )
+    path_depth_averaged_horizontalVelY_at_face = (
+        path_postprocess / "depth_averaged_horizontalVelY_at_face.nc"
+    )
+    ds_depth_averaged_horizontalVelY_at_face = xr.open_dataset(
+        path_depth_averaged_horizontalVelY_at_face, chunks=chunks
+    )
+
+    path_map = path_common / "grid_mapping_and_weight.nc"
+    ds_grid_mapping = xr.open_dataset(path_map, mask_and_scale=False)
+
+    logging.info("Read quantile data...")
+
+    chunks = {"time": 48}
+    path_temperature_quantile_at_face = path_common / "temperature_quantile_at_face.nc"
+    ds_temperature_quantile_at_face = xr.open_dataset(
+        path_temperature_quantile_at_face, chunks=chunks
+    )
+    da_temperature_quantile_at_face = (
+        ds_temperature_quantile_at_face.temperature_quantile_at_face
+    )
+    n_periods = da_temperature_quantile_at_face.time.size
+    timestamps = pd.date_range(start=f"{year}-07-01", freq="1D", periods=n_periods)
+    da_temperature_quantile_at_face = da_temperature_quantile_at_face.assign_coords(
+        time=timestamps
+    )
+    chunks = {"time": 48}
+    path_turbidity_quantile_at_face = path_common / "turbidity_quantile_at_face.nc"
+    ds_turbidity_quantile_at_face = xr.open_dataset(
+        path_turbidity_quantile_at_face, chunks=chunks
+    )
+    da_turbidity_quantile_at_face = (
+        ds_turbidity_quantile_at_face.turbidity_quantile_at_face
+    )
+    n_periods = da_turbidity_quantile_at_face.time.size
+    timestamps = pd.date_range(start=f"{year}-07-01", freq="1D", periods=n_periods)
+    da_turbidity_quantile_at_face = da_turbidity_quantile_at_face.assign_coords(
+        time=timestamps
+    )
+
+    da_daily_salinity_fraction_under_6_at_face = (
+        xr.where(
+            ds_depth_averaged_salinity_at_face.depth_averaged_salinity_at_face < 6.0,
+            1,
+            0,
+        )
+        .resample(time="1D", origin="start")
+        .mean()
+    )
+    da_daily_salinity_fraction_under_6_at_face = shift_30min_up(
+        da_daily_salinity_fraction_under_6_at_face
+    )
+    da_turbidity_levels = xr.where(da_turbidity_quantile_at_face > 12.0, 1, 0).sum(
+        dim="quantile"
+    )
+
+    da_probability = xr.apply_ufunc(
+        interpolate_turbidity_cutoff_probability,
+        da_turbidity_quantile_at_face,
+        da_turbidity_levels,
+        input_core_dims=[
+            [
+                "quantile",
+            ],
+            [],
+        ],
+        output_dtypes=float,
+        dask="parallelized",
+    )
+
+    da_temperature_levels = xr.where(da_temperature_quantile_at_face < 24.0, 1, 0).sum(
+        dim="quantile"
+    )
+    da_si_temperature = xr.apply_ufunc(
+        lambda v: np.array([0, 0.25, 0.5, 0.75, 1.0])[v],
+        da_temperature_levels,
+        dask="parallelized",
+    )
+    da_depth_averaged_hvel_mag_at_face = xr.apply_ufunc(
+        lambda v_x, v_y: np.hypot(v_x, v_y),
+        ds_depth_averaged_horizontalVelX_at_face.depth_averaged_horizontalVelX_at_face,
+        ds_depth_averaged_horizontalVelY_at_face.depth_averaged_horizontalVelY_at_face,
+        dask="parallelized",
+    )
+    da_depth_averaged_hvel_mag_daily_max_at_face = (
+        da_depth_averaged_hvel_mag_at_face.resample(time="1D", origin="start")
+        .max()
+    )
+    da_depth_averaged_hvel_mag_daily_max_at_face = shift_30min_up(
+        da_depth_averaged_hvel_mag_daily_max_at_face
+    )
+    da_hsi = (
+        0.67 * da_daily_salinity_fraction_under_6_at_face
+        + 0.33 * da_depth_averaged_hvel_mag_daily_max_at_face
+    )
+    da_hsi_final = (1.0 - 0.6 * da_probability) * da_hsi * da_si_temperature
+    da_hsi_final.name = "hsi"
+    da_hsi_final.attrs["units"] = "dimensionless"
+    da_hsi_final.attrs["long_name"] = "Habitat Suitability Index"
+    path_hsi = path_postprocess / "hsi.nc"
+    da_hsi_final.to_dataset().to_netcdf(path_hsi)
+
+
+def interpolate_turbidity_cutoff_probability(turb, levels):
+    """Interpolate turbidity probability at 12 NTU from turbidity quantile data"""
+    probability_all = np.zeros_like(turb)
+    # The last dimension through apply_ufnuc is the quantile dimension
+    funclist = [
+        lambda v1, v2: (12.0 - v1) / (v2 - v1) * 0.20 + 0.05,
+        lambda v1, v2: (12.0 - v1) / (v2 - v1) * 0.25 + 0.25,
+        lambda v1, v2: (12.0 - v1) / (v2 - v1) * 0.25 + 0.5,
+        lambda v1, v2: (12.0 - v2) / (v2 - v1) * 0.25 + 0.75,
+    ]
+    n_quantiles = turb.shape[-1]
+    for l in range(n_quantiles):
+        # For the last quantile, use the previous quantile set
+        l_ = l if l != n_quantiles - 1 else l - 1
+        v1 = turb[..., l_]
+        v2 = turb[..., l_ + 1]
+        probability_all[..., l] = funclist[l](v1, v2)
+    result = np.select(
+        [levels == i for i in range(n_quantiles)],
+        [probability_all[..., i] for i in range(n_quantiles)],
+        default=0.0,
+    )
+    np.clip(result, 0.0, 1.0, out=result)
+    return result
+
+
+def shift_30min_up(da):
+    """Shift time coordinate 30 minutes up"""
+    return da.assign_coords(time=da.coords["time"] - pd.to_timedelta("30m"))
+
+
+def read_out2d_and_create_grid(path_study, day_start, day_end):
+    logging.info("Reading out2d files...")
+    output_items = {
+        "out2d": "outputs/out2d_{}.nc",
+    }
+    chunks = {"time": 2}
+    list_nc_paths = {
+        k: [v.format(i) for i in range(day_start, day_end + 1)]
+        for k, v in output_items.items()
+    }
+
+    list_ds = []
+    for _, v in list_nc_paths.items():
+        list_paths = [str(path_study / v[i]) for i in range(len(v))]
+        ds = suxarray.helper.read_schism_nc(list_paths, chunks=chunks)
+        list_ds.append(ds)
+
+    ds = xr.merge(list_ds)
+    if sx.get_topology_variable(ds) is None:
+        ds = sx.add_topology_variable(ds)
+    ds = sx.coerce_mesh_name(ds)
+
+    logging.info("Creating a grid object...")
+    sx_ds = sx.Dataset(ds, sxgrid=sx.Grid(ds))
+    return sx_ds
+
+
+if __name__ == "__main__":
+    main()
---
+++ bdschism/bdschism/calculate_hsi_area.py
@@ -0,0 +1,103 @@
+"""
+"""
+
+from pathlib import Path
+import logging
+import argparse
+import pandas as pd
+import xarray as xr
+import suxarray as sx
+import suxarray.helper
+
+logging.basicConfig(
+    format="%(asctime)s %(levelname)s %(message)s",
+    datefmt="%Y-%m-%d %H:%M:%S",
+    level=logging.INFO,
+)
+
+
+def create_argparser():
+    argparser = argparse.ArgumentParser()
+    argparser.add_argument("--path_out2d", type=lambda s: Path(s))
+    argparser.add_argument("--path_common", type=lambda s: Path(s))
+    argparser.add_argument("--path_hsi", type=lambda s: Path(s))
+    argparser.add_argument(
+        "--path_out_nc", default="hsi_area.nc", type=lambda s: Path(s)
+    )
+    argparser.add_argument(
+        "--path_out_csv", default="hsi_area.csv", type=lambda s: Path(s)
+    )
+    return argparser
+
+
+def main():
+    argparser = create_argparser()
+    args = argparser.parse_args()
+
+    path_out2d = args.path_out2d
+    path_common = args.path_common
+    path_hsi = args.path_hsi
+    path_out_csv = args.path_out_csv
+    path_out_nc = args.path_out_nc
+
+    ds_out2d = suxarray.helper.read_schism_nc(path_out2d)
+    if sx.get_topology_variable(ds_out2d) is None:
+        ds_out2d = sx.add_topology_variable(ds_out2d)
+    ds_out2d = sx.coerce_mesh_name(ds_out2d)
+
+    logging.info("Creating a grid object...")
+    sx_ds = sx.Dataset(ds_out2d, sxgrid=sx.Grid(ds_out2d))
+
+    logging.info("Calculating face areas...")
+    da_face_areas = sx_ds.sxgrid.compute_face_areas()
+    da_face_areas.name = "face_area"
+    da_face_areas.attrs["units"] = "m2"
+    da_face_areas.attrs["long_name"] = "Face area"
+
+    logging.info("Reading habitat Suitablity Indices...")
+    chunks = {"time": 48}
+    ds_hsi = xr.open_dataset(path_hsi, chunks=chunks)
+    da_hsi = ds_hsi["hsi"].fillna(0.0)
+
+    path_subregions_nc = path_common / "subregion_hsi.nc"
+    ds_subregions = xr.open_dataset(path_subregions_nc)
+    ds_subregions = xr.decode_cf(ds_subregions)
+    ds_subregions = ds_subregions.rename({"nSCHISM_hgrid_face": "nMesh2_face"})
+
+    path_region_points = path_common / "region_pointsUTM.csv"
+    df_region_points = pd.read_csv(path_region_points, header=0)
+    list_regions = df_region_points["SUBREGION"].unique()
+
+    list_areas_hsi = []
+    list_total_areas = []
+    for region in list_regions:
+        da_elem_idx = ds_subregions[region]
+        da_face_areas_in_region = da_face_areas.where(da_elem_idx, drop=True)
+        da_area_hsi = xr.dot(
+            da_face_areas_in_region,
+            da_hsi.where(da_elem_idx, drop=True),
+            dims="nMesh2_face",
+        )
+        list_areas_hsi.append(da_area_hsi)
+        total_region_area = da_face_areas_in_region.sum()
+        list_total_areas.append(total_region_area)
+
+    da_area_hsi_all = xr.concat(list_areas_hsi, pd.Index(list_regions, name="region"))
+    da_area_hsi_all.name = "hsi_area"
+    da_area_hsi_all.attrs["units"] = "m2"
+    da_area_hsi_all.attrs["long_name"] = "HSI area for each region"
+    ds_area_hsi = da_area_hsi_all.to_dataset()
+
+    da_subareas = xr.concat(list_total_areas, pd.Index(list_regions, name="region"))
+    da_subareas.name = "subarea"
+    da_subareas.attrs["units"] = "m2"
+    ds_area_hsi = ds_area_hsi.update({"subarea": da_subareas})
+
+    ds_area_hsi.to_netcdf(path_out_nc)
+    ds_area_hsi.to_dataframe().to_csv(path_out_csv)
+
+    logging.info("Done...")
+
+
+if __name__ == "__main__":
+    main()
