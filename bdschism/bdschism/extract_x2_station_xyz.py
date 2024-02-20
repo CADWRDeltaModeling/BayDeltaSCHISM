ea0d689714648e555631965feebba202492c7b1f
Shu <Qiang.Shu@water.ca.gov>
2024-02-20 09:47:31 -0800

    updated x2 station extractor with more arguments and pep8 format, updated x2 cvs file

--- bdschism/bdschism/extract_x2_station_xyz.py
+++ bdschism/bdschism/extract_x2_station_xyz.py
@@ -8,95 +8,123 @@ upstream of San Joaquin River. Their depth are defined as relativly to surface. 
 stations are available, surface station set elevation to 0 and bottom station set elevation to -10000.0.
 station xyz are output as  Bay route, San Joaquin, Sac and MZM in order.
 
-usage: --x2route x2route.csv 
-output: x2 observation station xyz in SCHISM bp format
 """
 
 import numpy as np
-import datetime as dtm
-import matplotlib.dates as mdates
 import pandas as pd
 import argparse
 
 
+def create_arg_parser():
+    """ Create an argument parser
+        return: argparse.ArgumentParser
+    """
 
-    
-#plt.style.use(['seaborn-talk','seaborn-colorblind'])
-parser = argparse.ArgumentParser()
-parser.add_argument('--x2route', default=None,
-                        help='X2 route in cvs format')
+    # Read in the input file
+    parser = argparse.ArgumentParser(
+        description="""
+                       Convert X2 route dense point to SCHISM bp station format with specified sampling interval.
 
-args = parser.parse_args()
+                     Usage:
+                           extract_x2_station_xyz.py x2route.csv --out x2_stations.bp --sampling_interval 200 --bay_min_distance 65000 --sac_max_distance 25000 --sjr_max_distance 25000
+                     """)
+    parser.add_argument(
+        'x2route',
+        default=None,
+        help='csv file of x2 station location and distance')
 
+    parser.add_argument('--out', default="x2_station.bp", required=False,
+                        help='x2 schism station out file')
 
-x2_route_file=args.x2route
-#x2_route_file="x2route.csv"
+    parser.add_argument(
+        '--sampling_interval',
+        default=200,
+        required=False,
+        type=float,
+        help='sampling distance along original x2 route for SCHISM output stations in meter')
 
+    parser.add_argument(
+        '--bay_min_distance',
+        default=65000.0,
+        type=float,
+        required=False,
+        help='starting distance to sample station in the Bay measuring from San Francisco in meter')
 
-x2_route=pd.read_table(x2_route_file,sep=",")
+    parser.add_argument(
+        '--sac_max_distance',
+        default=25000,
+        type=float,
+        required=False,
+        help='end sampling distance distance to sample station in the Sacramento River measuring from the Confluence in meter')
 
-if x2_route.empty:
-    raise ValueError("no valid x2route.csv is given")
+    parser.add_argument(
+        '--sjr_max_distance',
+        default=25000,
+        type=float,
+        required=False,
+        help='end sampling distance distance to sample station in the San Joaquin River measuring from the Confluence in meter')
 
-
-surface_x2_xyz=0
-bottom_x2_xyz=0
-westmost_bay_dist=65000
-westmost_mzm_dist=18000
-eastmost_sac_dist=25000
-eastmost_sanjoaquin_dist=25000
-bay_points=x2_route.loc[(x2_route['RID']=='bay')&(x2_route['MEAS']>westmost_bay_dist)]
-mzm_points=x2_route.loc[(x2_route['RID']=='montezuma')&(x2_route['MEAS']>westmost_mzm_dist)]
-sanjoaquin_points=x2_route.loc[(x2_route['RID']=='san_joaquin')&(x2_route['MEAS']<eastmost_sanjoaquin_dist)]
-sac_points=x2_route.loc[(x2_route['RID']=='sacramento')&(x2_route['MEAS']<eastmost_sac_dist)]
-
-
-
-bay_distance=bay_points.loc[:,"SHAPE_LENG"].values[0]
-mzm_distance=mzm_points.loc[:,"SHAPE_LENG"].values[0]
-sanjoaquin_distance=sanjoaquin_points.loc[:,"SHAPE_LENG"].values[0]
-sac_distance=sac_points.loc[:,"SHAPE_LENG"].values[0]
-
-bay_pt_every_200m=range(0,len(bay_points),200)
-mzm_pt_every_500m=range(0,len(mzm_points),500)
-sanjoaquin_pt_every_200m=range(0,len(sanjoaquin_points),200)
-sac_pt_every_200m=range(0,len(sac_points),200)
-
-len_bay_pt=len(bay_pt_every_200m)
-len_mzm_pt=len(mzm_pt_every_500m)
-len_sanjoaquin_pt=len(sanjoaquin_pt_every_200m)
-len_sac_pt=len(sac_pt_every_200m)
-
-surface_num_stations=len_bay_pt+len_mzm_pt+len_sanjoaquin_pt+len_sac_pt
-bottom_num_stations=surface_num_stations
-
-
-bay_pt_meas=bay_points.iloc[bay_pt_every_200m]['MEAS'].values
-mzm_pt_meas=mzm_points.iloc[mzm_pt_every_500m]['MEAS'].values
-sanjoaquin_pt_meas=sanjoaquin_points.iloc[sanjoaquin_pt_every_200m]['MEAS'].values
-sac_pt_meas=sac_points.iloc[sac_pt_every_200m]['MEAS'].values
-
-surface_out_frame=pd.concat([bay_points.iloc[bay_pt_every_200m],sanjoaquin_points.iloc[sanjoaquin_pt_every_200m],sac_points.iloc[sac_pt_every_200m],mzm_points.iloc[mzm_pt_every_500m]])
-
-surface_out_frame["POINT_Z"]=0.0
+    return parser
 
 
-bottom_out_frame=pd.concat([bay_points.iloc[bay_pt_every_200m],sanjoaquin_points.iloc[sanjoaquin_pt_every_200m],sac_points.iloc[sac_pt_every_200m],mzm_points.iloc[mzm_pt_every_500m]])
-
-header=["POINT_X","POINT_Y","POINT_Z"]
-
-#out_frame.to_csv('x2_surface_stations.bp',columns=header,sep=" ",header=False)
-#print "total num of stations",len(out_frame)
-
-
-bottom_out_frame.loc[:,"POINT_Z"]=-10000.0
-
-x2_out_frame=pd.concat([surface_out_frame,bottom_out_frame])
-
-new_id=range(1,2*len(surface_out_frame)+1)
-
-x2_out_frame.index=new_id
-
-x2_out_frame.to_csv('x2_stations.bp',columns=header,sep=" ",header=False)
+def x2_route2_bp(x2_route_file, out, sample_interval, bay_min_distance, sanjoaquin_max_distance, sac_max_distance):
+    x2_route_file = "x2route.csv"
+    x2_route = pd.read_table(x2_route_file, sep=",", skiprows=1)
+    surface_x2_xyz = 0
+    bottom_x2_xyz = 0
 
 
+    bay_points_dist = x2_route.loc[(x2_route['path'] == 'bay')]["distance"]
+
+    bay_route_length = np.max(bay_points_dist)
+
+    sanjoaquin_max_distance = sanjoaquin_max_distance+bay_route_length
+    sac_max_distance = sac_max_distance+bay_route_length
+
+    bay_points = x2_route.loc[(x2_route['path'] == 'bay') & (
+        x2_route['distance'] > bay_min_distance)]
+    sanjoaquin_points = x2_route.loc[(x2_route['path'] == 'san_joaquin') & (
+        x2_route['distance'] < sanjoaquin_max_distance)]
+    sac_points = x2_route.loc[(x2_route['path'] == 'sacramento') & (
+        x2_route['distance'] < sac_max_distance)]
+
+    bay_pt_every_200m = range(0, len(bay_points), 200)
+    sanjoaquin_pt_every_200m = range(0, len(sanjoaquin_points), 200)
+    sac_pt_every_200m = range(0, len(sac_points), 200)
+
+    surface_out_frame = pd.concat([bay_points.iloc[bay_pt_every_200m],
+                                  sanjoaquin_points.iloc[sanjoaquin_pt_every_200m], sac_points.iloc[sac_pt_every_200m]])
+
+    surface_elev = 0.0
+    surface_out_frame["z"] = surface_elev
+
+    bottom_out_frame = pd.concat([bay_points.iloc[bay_pt_every_200m],
+                                 sanjoaquin_points.iloc[sanjoaquin_pt_every_200m], sac_points.iloc[sac_pt_every_200m]])
+
+    header = ["x", "y", "z"]
+
+    bottom_elev = -10000.0
+
+    bottom_out_frame.loc[:, "z"] = bottom_elev
+
+    x2_out_frame = pd.concat([surface_out_frame, bottom_out_frame])
+
+    new_id = range(1, 2*len(surface_out_frame)+1)
+
+    x2_out_frame.index = new_id
+
+    x2_out_frame.to_csv(out, columns=header, sep=" ", header=False)
+
+
+def main():
+    """ Main function
+    """
+    parser = create_arg_parser()
+    args = parser.parse_args()
+
+    x2_route2_bp(args.x2route, args.out, args.sampling_interval,
+                 args.bay_min_distance, args.sjr_max_distance, args.sac_max_distance)
+
+
+if __name__ == "__main__":
+    main()
