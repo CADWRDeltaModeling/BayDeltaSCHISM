e0eaf0b692fe583b718af44f9f3670cb4fddbdc7
lily-tomkovic-DWR <lily.tomkovic@water.ca.gov>
2025-03-11 10:15:43 -0700

    black formatting

--- test_suite/test_elev2d.py
+++ test_suite/test_elev2d.py
@@ -1,6 +1,4 @@
-""" Tests to make sure that the elev2d.th.nc file is referencing the correct start date
-"""
-
+"""Tests to make sure that the elev2d.th.nc file is referencing the correct start date"""
 
 import pytest
 import os
@@ -10,13 +8,14 @@ import datetime
 
 @pytest.mark.prerun
 def test_elev2d_time(sim_dir, params):
-    """ Reads elev2d.th.nc file and return start date, check against param.nml"""
-    elev_df = xr.open_dataset(os.path.join(sim_dir, 'elev2D.th.nc'))
-    elev_start = elev_df.time.values[0].astype(
-        'M8[ms]').astype(datetime.datetime)
+    """Reads elev2d.th.nc file and return start date, check against param.nml"""
+    elev_df = xr.open_dataset(os.path.join(sim_dir, "elev2D.th.nc"))
+    elev_start = elev_df.time.values[0].astype("M8[ms]").astype(datetime.datetime)
     param_start = params.run_start.to_pydatetime()
 
-    print(f'elev_start: {elev_start}')
-    print(f'param_start: {param_start}')
+    print(f"elev_start: {elev_start}")
+    print(f"param_start: {param_start}")
 
-    assert elev_start == param_start, f"elev2D.th.nc file start time {elev_start.strftime('%Y-%m-%d %H:%M')} does not match the param.nml start time {param_start.strftime('%Y-%m-%d %H:%M')}"
+    assert (
+        elev_start == param_start
+    ), f"elev2D.th.nc file start time {elev_start.strftime('%Y-%m-%d %H:%M')} does not match the param.nml start time {param_start.strftime('%Y-%m-%d %H:%M')}"
