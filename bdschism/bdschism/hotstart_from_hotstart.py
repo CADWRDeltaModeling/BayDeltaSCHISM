263956f8ab1ecddae2cf7eb1c2bb875228c49258
lily-tomkovic-DWR <Lily.Tomkovic@water.ca.gov>
2025-03-25 09:45:13 -0700

    black format

--- bdschism/bdschism/hotstart_from_hotstart.py
+++ bdschism/bdschism/hotstart_from_hotstart.py
@@ -14,8 +14,8 @@ import string
 import tempfile
 import argparse
 from pathlib import Path
-from datetime import datetime
 from collections import defaultdict
+from datetime import datetime
 
 # Third-Party Library Imports
 import pandas as pd
@@ -26,8 +26,10 @@ import schimpy.schism_hotstart as sh
 import schimpy.param as parms
 import bdschism.config as config
 
+
 ################# command line application #####################
 def str_or_path(value):
+    """Helper function to handle string or Path inputs."""
     return Path(value) if Path(value).exists() else value
 
 
@@ -60,20 +62,24 @@ def create_arg_parser():
     return parser
 
 class SafeDict(dict):
-    def __missing__(self, key):
-        return '{' + key + '}'
+    """Safe dictionary for string formatting."""
 
-def fmt_string_file(fn_in, fn_out, str_dict, method='format_map'):
-    with open(fn_in, 'r') as f:
+    def __missing__(self, key):
+        return "{" + key + "}"
+
+
+def fmt_string_file(fn_in, fn_out, str_dict, method="format_map"):
+    """Format a file using a dictionary of strings."""
+    with open(fn_in, "r") as f:
         fdata = f.read()
 
-    if method == 'format_map':
+    if method == "format_map":
         fdata = string.Formatter().vformat(fdata, (), SafeDict((str_dict)))
-    elif method == 'replace':
+    elif method == "replace":
         for key in str_dict.keys():
             fdata = fdata.replace(key, str_dict[key])
 
-    with open(fn_out, 'w') as fout:
+    with open(fn_out, "w") as fout:
         fout.write(fdata)
 
 def hotstart_newgrid(yaml_fn, hotstart_in, hotstart_out, in_dir, out_dir,
@@ -85,39 +91,46 @@ def hotstart_newgrid(yaml_fn, hotstart_in, hotstart_out, in_dir, out_dir,
     # get params
     param_nml_in = os.path.join(in_dir,'param.nml')
     params_in = parms.read_params(param_nml_in)
-    param_nml_out = os.path.join(out_dir,'param.nml')
+    param_nml_out = os.path.join(out_dir, "param.nml")
     params_out = parms.read_params(param_nml_out)
-    # baro = params.get_baro()
 
-    # get start date from hotstart file
+    # Get start date from hotstart file
     refds = xr.open_dataset(hotstart_in)
     run_start_in = params_in.run_start
-    hot_date = pd.Timedelta(seconds=refds['time'].values[0]) + run_start_in # used to fill strings in temp_yaml
-    timestep = params_out._namelist['CORE']['dt']['value'] # used to fill strings in temp_yaml
-    run_start = params_out.run_start # use runstart to pass to yaml
+    hot_date = (
+        pd.Timedelta(seconds=refds["time"].values[0]) + run_start_in
+    )  # used to fill strings in temp_yaml
+    timestep = params_out._namelist["CORE"]["dt"][
+        "value"
+    ]  # used to fill strings in temp_yaml
+    run_start = params_out.run_start  # use runstart to pass to yaml
 
     # set hgrid/vgrid in/outs
-    hgrid_in = os.path.join(in_dir, 'hgrid.gr3') # used to fill strings in temp_yaml
-    hgrid_out = os.path.join(out_dir, 'hgrid.gr3') # used to fill strings in temp_yaml
-    vgrid_in = os.path.join(in_dir, 'vgrid.in') # used to fill strings in temp_yaml
-    vgrid_out = os.path.join(out_dir, 'vgrid.in') # used to fill strings in temp_yaml
+    hgrid_in = os.path.join(in_dir, "hgrid.gr3")  # used to fill strings in temp_yaml
+    hgrid_out = os.path.join(out_dir, "hgrid.gr3")  # used to fill strings in temp_yaml
+    vgrid_in = os.path.join(in_dir, "vgrid.in")  # used to fill strings in temp_yaml
+    vgrid_out = os.path.join(out_dir, "vgrid.in")  # used to fill strings in temp_yaml
 
-    # create a temporary yaml filename
-    with tempfile.NamedTemporaryFile(mode="w", suffix=".yaml", delete=False) as temp_file:
+    # Create a temporary yaml filename
+    with tempfile.NamedTemporaryFile(
+        mode="w", suffix=".yaml", delete=False
+    ) as temp_file:
         temp_yaml = temp_file.name
         # uses **locals(): timestep, run_start, hot_date, hgrid_in, hgrid_out, vgrid_in, vgrid_out to populate yaml_fn into temp_yaml
-        fmt_string_file(yaml_fn, temp_yaml, SafeDict(**locals()), method='format_map') 
-        print(f'\t Temporary yaml filename: {temp_yaml}')
-    
-    # get modules used from hotstart infile
-    if modules is None:
-        ntracers, ntrs, irange_tr, modules = sh.describe_tracers(param_nml_out) # pull module list from param_nml
+        fmt_string_file(yaml, temp_yaml, SafeDict(**locals()), method="format_map")
+        print(f"\t Temporary yaml filename: {temp_yaml}")
 
-    # create a hotstart file for SCHISM
+    # Get modules used from hotstart infile
+    if modules is None:
+        ntracers, ntrs, irange_tr, modules = sh.describe_tracers(
+            param_nml_out
+        )  # pull module list from param_nml
+
+    # Create a hotstart file for SCHISM
     h = sh.hotstart(temp_yaml, modules=modules, crs=crs)
     h.create_hotstart()
     hnc = h.nc_dataset
-    hnc.to_netcdf(hotstart_out)   
+    hnc.to_netcdf(hotstart_out)
     os.remove(temp_yaml)
 
 def main():
