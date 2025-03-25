9db982b9551ad8d9824956f09ce1723e5ef97e2c
lily-tomkovic-DWR <Lily.Tomkovic@water.ca.gov>
2025-03-24 17:06:46 -0700

    convert set_nudging to click from argparse

--- bdschism/bdschism/set_nudging.py
+++ bdschism/bdschism/set_nudging.py
@@ -5,9 +5,9 @@ Set the nudging links for a Bay-Delta SCHSIM model
 
 2025-02-04: Customized
 """
+import click
 
 from datetime import datetime
-import argparse
 
 import schimpy.param as parms
 import bdschism.settings as config
@@ -16,82 +16,90 @@ import os
 
 ################# command line application #####################
 
-def create_arg_parser():
-    import textwrap
-    parser = argparse.ArgumentParser(
-        formatter_class=argparse.RawDescriptionHelpFormatter,
-        epilog=textwrap.dedent(
-            """
-         ============== Example ==================
-      > set_nudging.py --suffix obshycom --workdir .
-      """),
-        description="""The script will identify nudging gr3 files and point the schism names to the suffix names."""
-    )
-    parser.add_argument('--suffix', default=None, required=True,
-                        help="the suffix desired for the nudging/gr3 files. Ex: 'obshycom' in SAL_nu_obshycom.nc")
-    parser.add_argument('--workdir', default='.', required=False, 
-                        help='Simulation directory path')
-    parser.add_argument('--var_map', default="", type=parse_var_map, required=False, 
-                        help="Any unexpected mapping in key=value pairs separated by commas. Ex: --var_map 'TEM=temperature,SAL=salinity'")
-    return parser
 
-def parse_var_map(s):
+def parse_var_map(ctx, param, value):
     """
     Parse a string of key=value pairs separated by commas into a dictionary.
     Expected format: "key1=value1,key2=value2"
     Used for var_map which feeds the gr3 formats for things like TEM=temperature
     """
     mapping = {}
-    if s!="":
+    if value:  # Check if the value is not empty
         # Remove any surrounding quotes or spaces and split by comma
-        pairs = s.split(',')
+        pairs = value.split(",")
         for pair in pairs:
             print(pair)
-            if '=' not in pair:
-                raise argparse.ArgumentTypeError(
+            if "=" not in pair:
+                raise click.BadParameter(
                     f"Invalid format for var_map: '{pair}'. Expected format key=value."
                 )
-            key, value = pair.split('=', 1)
+            key, value = pair.split("=", 1)
             mapping[key.strip()] = value.strip()
     return mapping
 
+
 def get_nudge_list(workdir):
-    fname = os.path.join(workdir,"param.nml")
-    
+    fname = os.path.join(workdir, "param.nml")
+
     params = parms.read_params(fname)
     nc_nudge_list = []
-    nc_dict = {1: 'TEM',
-               2: 'SAL',
-               3: 'GEN',
-               4: 'AGE',
-               5: 'SED',
-               6: 'ECO',
-               7: 'ICM',
-               8: 'COS',
-               9: 'FIB',
-               10: 'TIMOR-NOT-ACTIVE',
-               11: 'FBM'}
+    nc_dict = {
+        1: "TEM",
+        2: "SAL",
+        3: "GEN",
+        4: "AGE",
+        5: "SED",
+        6: "ECO",
+        7: "ICM",
+        8: "COS",
+        9: "FIB",
+        10: "TIMOR-NOT-ACTIVE",
+        11: "FBM",
+    }
 
     for i in range(1, 12):
-        if params._namelist['OPT'][f'inu_tr({i})']['value'] == 2:
+        if params._namelist["OPT"][f"inu_tr({i})"]["value"] == 2:
             nc_nudge_list.append(nc_dict[i])
 
     return nc_nudge_list
 
-def set_nudging(suffix, workdir='.', var_map={}):
-    """ This is a utility to set up nudging files based on a naming convention common for BayDeltaSCHISM. 
+
+@click.command(
+    help=(
+        "Set the nudging module files for SCHISM. Usage: 'set_nudging roms'\n\n"
+        "Arguments:\n"
+        "  SUFFIX  This is the suffix used when preparing the nudging/gr files. "
+        "For instance 'obshycom' in SAL_nu_obshycom.nc"
+    )
+)
+@click.argument("suffix")
+@click.option(
+    "--workdir", default=".", show_default=True, help="Simulation directory path."
+)
+@click.option(
+    "--var_map",
+    default="",
+    callback=parse_var_map,
+    help="Unexpected mapping in key=value pairs, separated by commas. Ex: --var_map 'TEM=temperature,SAL=salinity'",
+)
+@click.help_option("-h", "--help")  # Add this line to enable both -h and --help
+def set_nudging(suffix: str, workdir=".", var_map={}):
+    """This is a utility to set up nudging files based on a naming convention common for BayDeltaSCHISM.
     Assumed this is on Linux or admin-priveleged Windows machine.
-    
-    Parameters
+
+    Arguments
     ---------
-    
+
     suffix: str
         This is the suffix used when preparing the nudging/gr files. For instance "obshycom" in SAL_nu_obshycom.nc
-        
-    workdir : str
-        Directory where the links and changes are made        
 
-    """ 
+    workdir : str
+        Directory where the links and changes are made
+
+    var_map: str
+        Unexpected mapping in key=value pairs, separated by commas. Ex: --var_map 'TEM=temperature,SAL=salinity'
+
+    """
 
     nc_nudge_list = get_nudge_list(workdir)
     check_files = []
@@ -112,35 +120,34 @@ def set_nudging(suffix, workdir='.', var_map={}):
         var_gr3_out = "{MOD}_nudge.gr3".format(**locals())
         var_nc_out = "{MOD}_nu.nc".format(**locals())
 
-        print(f"\t{gr3_color}{MOD} .gr3{reset_color}: Linked {var_gr3_out} to {var_gr3_in}")
-        config.create_link(var_gr3_in, os.path.join(workdir,var_gr3_out) )
+        print(
+            f"\t{gr3_color}{MOD} .gr3{reset_color}: Linked {var_gr3_out} to {var_gr3_in}"
+        )
+        config.create_link(var_gr3_in, os.path.join(workdir, var_gr3_out))
         print(f"\t{nc_color}{MOD} .nc{reset_color}: Linked {var_nc_out} to {var_nc_in}")
-        config.create_link(var_nc_in, os.path.join(workdir,var_nc_out))
+        config.create_link(var_nc_in, os.path.join(workdir, var_nc_out))
 
-    invalid_files = [cf for cf in check_files if not os.path.exists(os.path.join(workdir, cf))]
+    invalid_files = [
+        cf for cf in check_files if not os.path.exists(os.path.join(workdir, cf))
+    ]
 
     if invalid_files:
-        red_color = "\033[91m"   # Red for main message
+        red_color = "\033[91m"  # Red for main message
         pink_color = "\033[95m"  # Pink for filenames
 
         error_message = (
             f"{red_color}The following files are not in the directory:\n"
-            + "\n".join(f"\t{pink_color}- {file}{reset_color}" for file in invalid_files)
+            + "\n".join(
+                f"\t{pink_color}- {file}{reset_color}" for file in invalid_files
+            )
         )
 
         raise ValueError(error_message)
 
 
 def main():
-    parser = create_arg_parser()
-    args = parser.parse_args()
+    set_nudging()
 
-    suffix = args.suffix
-    workdir = args.workdir
-    var_map = args.var_map
-    
-    return set_nudging(suffix, workdir='.',var_map=var_map)
 
 if __name__ == "__main__":
-    main()
-    # set_nudging('roms', workdir='path_to_schism_sim_dir')
+    set_nudging()
