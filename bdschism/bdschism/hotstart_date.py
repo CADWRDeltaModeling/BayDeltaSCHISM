e958b79313fba94c3ad7b46d13370c97cbd7b0b6
lily-tomkovic-DWR <Lily.Tomkovic@water.ca.gov>
2025-03-25 10:38:24 -0700

    hot_date to click

--- bdschism/bdschism/hotstart_date.py
+++ bdschism/bdschism/hotstart_date.py
@@ -10,87 +10,75 @@
 
 import xarray as xr
 import pandas as pd
-import argparse
+import click
 from pathlib import Path
 
-def create_arg_parser():
-    import textwrap
-    parser = argparse.ArgumentParser(
-        formatter_class=argparse.RawDescriptionHelpFormatter,
-        epilog=textwrap.dedent(
-            """
-         ============== Example ==================
-      > hot_date --fn ./hotstart.nc --run_start 2024-6-24 --restart_time 2024-6-24
-      """),
-        description="""The script will change the necessary date attributes"""
+
+@click.command(
+    help=(
+        "The script will change the necessary date attributes in a hotstart file.\n\n"
+        "Example:\n"
+        "  hot_date --fn ./hotstart.nc --run_start 2024-6-24 --restart_time 2024-6-24"
     )
-    
-    parser.add_argument('--fn', default='./hotstart.nc', required=False, type=Path,
-                        help="hotstart *.nc filename")
-    parser.add_argument('--run_start', default=None, required=True, type=str,
-                        help="run start date in format YEAR-MONTH-DAY, ex: 2024-6-1")
-    parser.add_argument('--restart_time', default=None, required=True, type=str, 
-                        help="hotstart restart date in format YEAR-MONTH-DAY, ex: 2024-6-24")
-    parser.add_argument('--outprefix', default="hotstart", required=False, type=str,
-                        help='output prefix for the output .nc file. If hotprefix is just "hot" you would get hot.20240624.109440')
-    parser.add_argument('--dt', default=90, required=False, type=int,
-                        help="timestep, default is 90 seconds")
-    return parser
-
-def set_hotstart_date(filenm,outprefix,run_start,restart_time,dt):
-    """ Change timestamp and date implied by hotstart 
-    
-    Arguments
-    ---------
-    filenm : str
-    Original hotstart file name
-    
-    outprefix : str
-    Prefix for output hotstart. Date and nsteps will be appended. So if hotprefix is just "hot" you 
-    would get hot.20210425.109440
-    
-    run_start : str or Timestamp
-    Timestamp representing nominal start of the run (=0 elapsed time)
-
-    restart_time : str or Timestamp
-    Time stamp you want to restart the model
-    
-    dt : float
-    Time step of the model in seconds
-    """
+)
+@click.option(
+    "--fn",
+    default="./hotstart.nc",
+    type=click.Path(exists=True, path_type=Path),
+    help="Hotstart *.nc filename. [default: ./hotstart.nc]",
+)
+@click.option(
+    "--run_start",
+    required=True,
+    type=str,
+    help="Run start date in format YEAR-MONTH-DAY, e.g., 2024-6-1.",
+)
+@click.option(
+    "--restart_time",
+    required=True,
+    type=str,
+    help="Hotstart restart date in format YEAR-MONTH-DAY, e.g., 2024-6-24.",
+)
+@click.option(
+    "--outprefix",
+    default="hotstart",
+    type=str,
+    help=(
+        "Output prefix for the output .nc file. If outprefix is just 'hot', "
+        "you would get hot.20240624.109440. [default: hotstart]"
+    ),
+)
+@click.option(
+    "--dt",
+    default=90,
+    type=int,
+    help="Timestep in seconds. [default: 90]",
+)
+def set_hotstart_date(fn, run_start, restart_time, outprefix, dt):
+    """Change timestamp and date implied by hotstart."""
     run_start = pd.to_datetime(run_start)
     run_start_str = run_start.strftime("%Y-%m-%dT%H:%M")
     restart_time = pd.to_datetime(restart_time)
-    
-    
+
     restart_sec = (restart_time - run_start).total_seconds()
     restart_timestr = restart_time.strftime("%Y%m%d")
-    nsteps = int(restart_sec/dt)
+    nsteps = int(restart_sec / dt)
     outfile = f"{outprefix}.{restart_timestr}.{nsteps}.nc"
-    print(f"Restarting on {restart_time}. nsteps (iterations) = {nsteps}, elapsed secs at restart = {restart_sec}")
-    print(f"Time origin of run is {run_start_str}")
-    print(f"Output file is {outfile}")
-    
-    with xr.open_dataset(filenm,   ) as ds:
+
+    click.echo(
+        f"Restarting on {restart_time}. nsteps (iterations) = {nsteps}, elapsed secs at restart = {restart_sec}"
+    )
+    click.echo(f"Time origin of run is {run_start_str}")
+    click.echo(f"Output file is {outfile}")
+
+    with xr.open_dataset(fn) as ds:
         ds.variables["time"][:] = restart_sec
         ds.variables["nsteps_from_cold"][:] = nsteps
         ds.variables["iths"][:] = nsteps
         ds.variables["ifile"][:] = 1
-        ds.attrs['time_origin_of_simulation'] = run_start_str
+        ds.attrs["time_origin_of_simulation"] = run_start_str
         ds.to_netcdf(outfile)
-    
 
-def main():
-    parser = create_arg_parser()
-    args = parser.parse_args()
 
-    fn = args.fn
-    run_start = args.run_start
-    restart_time = args.restart_time
-    dt = float(args.dt)
-    outprefix = args.outprefix
-
-    return set_hotstart_date(fn,outprefix,run_start,restart_time,dt)
-
-if __name__=="__main__":
-    main()
+if __name__ == "__main__":
+    set_hotstart_date()
