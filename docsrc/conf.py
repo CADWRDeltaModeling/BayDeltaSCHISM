09885fa67b6ad15ce213bee849bd9e94e7be90ad
lily-tomkovic-DWR <Lily.Tomkovic@water.ca.gov>
2025-04-18 12:21:23 -0700

    allow style.css to influence html output

--- docsrc/conf.py
+++ docsrc/conf.py
@@ -156,6 +156,7 @@ html_logo = "./_static/logo6.png"
 # relative to this directory. They are copied after the builtin static files,
 # so a file named "default.css" will overwrite the builtin "default.css".
 html_static_path = ["_static"]
+html_css_files = ["style.css"]
 
 # If not '', a 'Last updated on:' timestamp is inserted at every page bottom,
 # using the given strftime format.
