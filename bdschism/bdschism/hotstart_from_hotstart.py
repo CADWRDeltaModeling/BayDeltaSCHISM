388ad781c092dc0c39be709c629706558dc9f36c
lily-tomkovic-DWR <Lily.Tomkovic@water.ca.gov>
2025-06-05 14:07:09 -0700

    utilize new schism_yaml functionality and util

--- bdschism/bdschism/hotstart_from_hotstart.py
+++ bdschism/bdschism/hotstart_from_hotstart.py
@@ -9,7 +9,6 @@ Adapted from ZZheng - generate hotstart - transfer from one grid to another
 # Standard Library Imports
 import os
 import string
-import tempfile
 from pathlib import Path
 from collections import defaultdict
 from datetime import datetime
@@ -18,9 +17,11 @@ from datetime import datetime
 import pandas as pd
 import xarray as xr
 import click
+import yaml
 
 # Project-Specific Imports
 import schimpy.schism_hotstart as sh
+from schimpy.util.yaml_load import yaml_from_file
 import schimpy.param as parms
 import bdschism.config as config
 
@@ -31,106 +32,83 @@ def str_or_path(value):
     return Path(value) if Path(value).exists() else value
 
 
-class SafeDict(dict):
-    """Safe dictionary for string formatting."""
-
-    def __missing__(self, key):
-        return "{" + key + "}"
-
-
-def fmt_string_file(fn_in, fn_out, str_dict, method="format_map"):
-    """Format a file using a dictionary of strings."""
-    with open(fn_in, "r") as f:
-        fdata = f.read()
-
-    if method == "format_map":
-        fdata = string.Formatter().vformat(fdata, (), SafeDict((str_dict)))
-    elif method == "replace":
-        for key in str_dict.keys():
-            fdata = fdata.replace(key, str_dict[key])
-
-    with open(fn_out, "w") as fout:
-        fout.write(fdata)
-
-
 def hotstart_newgrid(
-    yaml: str,
+    yaml_fn: str,
     hotstart_in,
     hotstart_out,
     src_dir,
     trg_dir,
     modules=None,
     crs="EPSG:26910",
+    envvar=None,
 ):
     """Transfer hotstart data from one grid to another."""
 
-    # Check that all files are available
-    param_nml_in = os.path.join(src_dir, "param.nml")
-    param_nml_out = os.path.join(trg_dir, "param.nml")
-    hgrid_in = os.path.join(src_dir, "hgrid.gr3")  # used to fill strings in temp_yaml
-    hgrid_out = os.path.join(trg_dir, "hgrid.gr3")  # used to fill strings in temp_yaml
-    vgrid_in = os.path.join(src_dir, "vgrid.in")  # used to fill strings in temp_yaml
-    vgrid_out = os.path.join(trg_dir, "vgrid.in")  # used to fill strings in temp_yaml
-    
-    check_dirs = [src_dir, trg_dir]
-    check_files = [hotstart_in, yaml, param_nml_in, param_nml_out, hgrid_in, hgrid_out, vgrid_in, vgrid_out]
-    # Ensure directories exist
-    missing_directories = [directory for directory in check_dirs if not os.path.isdir(directory)]
-    missing_files = [file for file in check_files if not os.path.isfile(file)]
+    # Build standard hotstart transfer dictionary
+    repl_dict = {
+        "hotstart_in": str(hotstart_in),
+        "param_nml_in": os.path.join(src_dir, "param.nml"),
+        "param_nml_out": os.path.join(trg_dir, "param.nml"),
+        "hgrid_in": os.path.join(src_dir, "hgrid.gr3"),
+        "hgrid_out": os.path.join(trg_dir, "hgrid.gr3"),
+        "vgrid_in": os.path.join(src_dir, "vgrid.in"),
+        "vgrid_out": os.path.join(trg_dir, "vgrid.in"),
+        "src_dir": str(src_dir),
+        "trg_dir": str(trg_dir),
+    }
+    if envvar is not None:
+        if not isinstance(envvar, dict):
+            raise ValueError("envvar must be a dictionary if provided.")
+        repl_dict.update(envvar)
 
-    if missing_directories or missing_files:
-        if missing_directories:
-            print("Missing directories:")
-            for directory in missing_directories:
-                print(f"  - {directory}")
-        if missing_files:
-            print("Missing files:")
-            for file in missing_files:
-                print(f"  - {file}")
-        raise FileNotFoundError("Some required directories or files are missing. See the output above for details.")
+    # Ensure files and directories exist
+    missing_filesdirs = [f for f in repl_dict.values() if not os.path.exists(f)]
+
+    if missing_filesdirs:
+        print("Missing directories or files:")
+        for filedir in missing_filesdirs:
+            print(f"  - {filedir}")
+        raise FileNotFoundError(
+            "Some required directories or files are missing. See the output above for details."
+        )
+    repl_dict["hotstart_out"] = str(hotstart_out)
 
     # Get params
-    params_in = parms.read_params(param_nml_in)
-    params_out = parms.read_params(param_nml_out)
+    params_in = parms.read_params(repl_dict["param_nml_in"])
+    params_out = parms.read_params(repl_dict["param_nml_out"])
 
     # Get start date from hotstart file
     refds = xr.open_dataset(hotstart_in)
     run_start_in = params_in.run_start
-    hot_date = (
+    repl_dict["hot_date"] = (
         pd.Timedelta(seconds=refds["time"].values[0]) + run_start_in
-    )  # used to fill strings in temp_yaml
-    timestep = params_out._namelist["CORE"]["dt"][
-        "value"
-    ]  # used to fill strings in temp_yaml
-    run_start = params_out.run_start  # use runstart to pass to yaml
-
-    # Create a temporary yaml filename
-    with tempfile.NamedTemporaryFile(
-        mode="w", suffix=".yaml", delete=False
-    ) as temp_file:
-        temp_yaml = temp_file.name
-        # uses **locals(): timestep, run_start, hot_date, hgrid_in, hgrid_out, vgrid_in, vgrid_out to populate yaml_fn into temp_yaml
-        fmt_string_file(yaml, temp_yaml, SafeDict(**locals()), method="format_map")
-        print(f"\t Temporary yaml filename: {temp_yaml}")
+    ).strftime(
+        "%Y-%m-%d %H:%M"
+    )  # used to fill strings in yaml
+    repl_dict["timestep"] = str(
+        int(params_out._namelist["CORE"]["dt"]["value"])
+    )  # used to fill strings in yaml
+    repl_dict["run_start"] = params_out.run_start.strftime(
+        "%Y-%m-%d %H:%M"
+    )  # use runstart to pass to yaml
 
     # Get modules used from hotstart infile
     if modules is None:
         ntracers, ntrs, irange_tr, modules = sh.describe_tracers(
-            param_nml_out
+            repl_dict["param_nml_out"]
         )  # pull module list from param_nml
 
     # Create a hotstart file for SCHISM
-    h = sh.hotstart(temp_yaml, modules=modules, crs=crs)
+    h = sh.hotstart(yaml_fn, modules=modules, crs=crs, envvar=repl_dict)
     h.create_hotstart()
     hnc = h.nc_dataset
     # Add the YAML file content as an entry to the netCDF file
-    with open(temp_yaml, "r") as yaml_file:
-        yaml_content = yaml_file.read()
-        
-    hnc.attrs["yaml_input"] = yaml_content
-    os.remove(temp_yaml)
+    yaml_content = yaml_from_file(yaml_fn, envvar=repl_dict)
+    yaml_str = yaml.safe_dump(yaml_content, sort_keys=False)
+    hnc.attrs["yaml_input"] = yaml_str
 
     # Write out the hotstart file to NetCDF
+    print(f"Writing hotstart file to {hotstart_out}")
     hnc.to_netcdf(hotstart_out)
 
 
