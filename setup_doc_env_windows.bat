864bb6acf58a55aaa52e8fea37b68f3f771eef37
lily-tomkovic-DWR <Lily.Tomkovic@water.ca.gov>
2025-06-18 15:21:35 -0700

    deal with linux libfortran needs v. windows

--- setup_doc_env_windows.bat
+++ setup_doc_env_windows.bat
@@ -2,6 +2,13 @@
 @REM to set up your documentation environment
 @REM this works in windows by simply calling ./setup_doc_env_windows.bat in a terminal with conda initialized
 
+@echo off
+REM Check if running on Windows
+if /i not "%OS%"=="Windows_NT" (
+    echo This script is intended for Windows only. Exiting.
+    exit /b 1
+)
+
 REM Check if the "bds_doc" environment already exists
 conda env list | findstr "bds_doc" > nul
 if %errorlevel% equ 0 (
@@ -9,31 +16,32 @@ if %errorlevel% equ 0 (
     exit /b 1
 ) else (
     REM Activate base environment
-    conda activate base
+    conda activate base || exit /b
 
     REM Create the "bds_doc" environment from the YAML file
-    conda env create -n bds_doc -f .\schism_env.yml
+    conda env create -n bds_doc -f .\schism_env.win.yml || exit /b
 
     REM Activate the "bds_doc" environment
-    conda activate bds_doc
+    conda activate bds_doc || exit /b
 
     REM Configure npm registry and disable strict SSL
-    npm config set registry http://registry.npmjs.org/
-    npm config set strict-ssl false
+    npm config set registry http://registry.npmjs.org/ || exit /b
+    npm config set strict-ssl false || exit /b
 
     REM Install Mermaid CLI globally
-    npm install -g @mermaid-js/mermaid-cli
+    npm install -g @mermaid-js/mermaid-cli || exit /b
 
     REM Pull the latest changes from the repository
-    git pull --rebase
-
-    REM Install version-specific uxarray and suxarray tools
-    git clone -b suxarray https://github.com/kjnam/uxarray.git && pushd uxarray && pip install --no-deps . && popd && rmdir .\uxarray\ -Recurse -Force
-
-    git clone -b v2024.09.0 https://github.com/cadwrdeltamodeling/suxarray && pushd suxarray && pip install --no-deps . && popd && rmdir .\suxarray\ -Recurse -Force
+    git pull --rebase || exit /b
 
     REM Install the bdschism package with documentation dependencies
-    pip install -e ./bdschism[doc]
+    pip install --trusted-host pypi.org --trusted-host pypi.python.org --trusted-host files.pythonhosted.org -e ./bdschism[doc] || exit /b
+
+    REM Install version-specific uxarray and suxarray tools
+    git clone -b suxarray https://github.com/kjnam/uxarray.git && pushd uxarray && pip install --trusted-host pypi.org --trusted-host pypi.python.org --trusted-host files.pythonhosted.org --no-deps . && popd && rmdir .\uxarray\ -Recurse -Force || exit /b
+
+    git clone -b v2024.09.0 https://github.com/cadwrdeltamodeling/suxarray && pushd suxarray && pip install --trusted-host pypi.org --trusted-host pypi.python.org --trusted-host files.pythonhosted.org --no-deps . && popd && rmdir .\suxarray\ -Recurse -Force || exit /b
+
 )
 
 REM Ensure the terminal ends with the "bds_doc" environment active
