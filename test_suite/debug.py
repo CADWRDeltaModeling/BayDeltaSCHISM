4a56a902cca6517dadefc1834951e187e995f66d
lily-tomkovic-DWR <lily.tomkovic@water.ca.gov>
2025-06-25 13:51:16 -0700

    create debug space for test suite

---
+++ test_suite/debug.py
@@ -0,0 +1,20 @@
+# import os
+# import xarray as xr
+import numpy as np
+import datetime
+import schimpy.param as parms
+from test_elev2d import test_elev2d_time
+import test_nudging
+import test_open_boundary
+import test_smscg
+import test_source_sink
+import test_vgrid
+import conftest
+
+if __name__ == "__main__":
+    # code here
+    sim_dir = "../examples/test_files"
+    fname = os.path.join(sim_dir, "param.nml")
+    params = parms.read_params(fname)
+
+    test_elev2d_time(sim_dir, params)
