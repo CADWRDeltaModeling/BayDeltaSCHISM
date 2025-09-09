11f62fa73d5271cdcaed08ee6232ceb8f2c5d62f
lily-tomkovic-DWR <Lily.Tomkovic@water.ca.gov>
2025-09-09 14:51:18 -0700

    fix versioning root, looks for .git folder

--- bdschism/bdschism/__init__.py
+++ bdschism/bdschism/__init__.py
@@ -18,6 +18,6 @@ except PackageNotFoundError:
     try:
         from setuptools_scm import get_version
 
-        __version__ = get_version(root="..", relative_to=__file__)
+        __version__ = get_version(root="../..", relative_to=__file__)
     except Exception:
         __version__ = "unknown"
