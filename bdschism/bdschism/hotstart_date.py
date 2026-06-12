7c0a1f79d3bce1f57c8fb1f77526968fdf377315
Eli <Eli.Ateljevich@water.ca.gov>
2026-06-11 19:25:05 -0700

    Added logging to hotstart_date.py

--- bdschism/bdschism/hotstart_date.py
+++ bdschism/bdschism/hotstart_date.py
@@ -2,16 +2,20 @@
 # -*- coding: utf-8 -*-
 
 # Use example as command-line interface function:
-# python hotstart_date.py --fn ./hotstart.nc --hotstart 2024-6-24 --runstart 2024-6-24
+# python hotstart_date.py --fn ./hotstart.nc --hotstart 2024-06-24 --runstart 2024-06-24
 
 # or with bdschism in your environment::
 
-# hot_date --fn ./hotstart.nc --hotstart 2024-6-24 --runstart 2024-6-24
+# hot_date --fn ./hotstart.nc --hotstart 2024-06-24 --runstart 2024-06-24
+
+import logging
 
 import xarray as xr
 import pandas as pd
 import click
 from pathlib import Path
+
+logger = logging.getLogger(__name__)
 
 
 def set_hotstart_date(fn, run_start, restart_time, outprefix, dt):
@@ -25,11 +29,12 @@ def set_hotstart_date(fn, run_start, restart_time, outprefix, dt):
     nsteps = int(restart_sec / dt)
     outfile = f"{outprefix}.{restart_timestr}.{nsteps}.nc"
 
-    print(
-        f"Restarting on {restart_time}. nsteps (iterations) = {nsteps}, elapsed secs at restart = {restart_sec}"
+    logger.info(
+        "Restarting on %s. nsteps (iterations) = %d, elapsed secs at restart = %g",
+        restart_time, nsteps, restart_sec,
     )
-    print(f"Time origin of run is {run_start_str}")
-    print(f"Output file is {outfile}")
+    logger.info("Time origin of run is %s", run_start_str)
+    logger.info("Output file is %s", outfile)
 
     with xr.open_dataset(fn) as ds:
         ds.variables["time"][:] = restart_sec
@@ -44,7 +49,7 @@ def set_hotstart_date(fn, run_start, restart_time, outprefix, dt):
     help=(
         "The script will change the necessary date attributes in a hotstart file.\n\n"
         "Example:\n"
-        "  hot_date --fn ./hotstart.nc --run_start 2024-6-24 --restart_time 2024-6-24"
+        "  hot_date --fn ./hotstart.nc --run_start 2024-06-24 --restart_time 2024-06-24"
     )
 )
 @click.option(
@@ -57,13 +62,13 @@ def set_hotstart_date(fn, run_start, restart_time, outprefix, dt):
     "--run_start",
     required=True,
     type=str,
-    help="Run start date in format YEAR-MONTH-DAY, e.g., 2024-6-1.",
+    help="Run start date in format YEAR-MONTH-DAY, e.g., 2024-06-01.",
 )
 @click.option(
     "--restart_time",
     required=True,
     type=str,
-    help="Hotstart restart date in format YEAR-MONTH-DAY, e.g., 2024-6-24.",
+    help="Hotstart restart date in format YEAR-MONTH-DAY, e.g., 2024-06-24.",
 )
 @click.option(
     "--outprefix",
@@ -80,9 +85,29 @@ def set_hotstart_date(fn, run_start, restart_time, outprefix, dt):
     type=int,
     help="Timestep in seconds. [default: 90]",
 )
+@click.option(
+    "--logdir",
+    default=None,
+    type=click.Path(),
+    help="Directory for log files.",
+)
+@click.option(
+    "--debug",
+    is_flag=True,
+    default=False,
+    help="Enable debug logging.",
+)
 @click.help_option("-h", "--help")
-def set_hotstart_date_cli(fn, run_start, restart_time, outprefix, dt):
+def set_hotstart_date_cli(fn, run_start, restart_time, outprefix, dt, logdir, debug):
     """CLI for setting hotstart date."""
+    from bdschism.logging_config import configure_logging
+
+    configure_logging(
+        package_name="bdschism",
+        level=logging.DEBUG if debug else logging.INFO,
+        logdir=Path(logdir) if logdir else None,
+        logfile_prefix="hot_date",
+    )
     set_hotstart_date(fn, run_start, restart_time, outprefix, dt)
 
 
